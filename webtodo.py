f8534ad4297f8d05141360e82a07d1d297f4f33d
lucky2504 <laxminarayanadoosa@gmail.com>
2025-06-01 16:50:30 -0700

    Adding a todo

--- webtodo.py
+++ webtodo.py
@@ -6,6 +6,11 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 st.title("My Todo App")
 st.subheader("This is my todoapp!")
 st.write('To increase productivity!')
@@ -13,5 +18,7 @@ st.write('To increase productivity!')
 for todo in todos:
     st.checkbox(todo)
 
-
-st.text_input(label="Enter a todo: ", placeholder = "Add a new todo...")
+st.text_input(label="Enter a todo: ", placeholder = "Add a new todo...",
+              on_change=add_todo, key='new_todo')
+print("Hello!")
+st.session_state
